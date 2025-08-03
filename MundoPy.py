47f3e0b80e7ea07edf66fb62a70762dfb32aa742
Brey007 <sirope44@gmail.com>
2025-08-03 18:56:05 -0500

    Recordatorios
    Recordamos los tipos de variables numericos y las operaciones matematicas basicas que podemos hacer en python.

---
+++ MundoPy.py
@@ -0,0 +1,22 @@
+print ('\nBIENVENIDOS A MIS PRACTICAS INSANAS')
+
+a = 10
+b = 3
+
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+divisionEntera = a // b
+modulo = a % b
+potencia = a ** b
+
+print ('\nSuma: ', suma)
+print ('Resta: ', resta)
+print ('Multiplicacion: ', multiplicacion)
+print ('Division: ', division)
+print ('Division entera: ', divisionEntera)
+print ('Modulo(residuo): ', modulo)
+print ('Potencia: ', potencia)
+
+print (' ')
