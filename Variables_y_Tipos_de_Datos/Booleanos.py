8e901737118373056eeb95d44d4ad100e2c899e4
Brey007 <sirope44@gmail.com>
2025-08-09 22:01:29 -0500

    Booleanos operaciones logicas

--- Variables_y_Tipos_de_Datos/Booleanos.py
+++ Variables_y_Tipos_de_Datos/Booleanos.py
@@ -4,9 +4,17 @@ y = 3
 print(x > y)  # True  (5 es mayor que 3)
 print(x == y) # False (5 no es igual a 3)
 
+# Operadores de comparacion
+
 # ==	Igual a	5 == 5	True
 # !=	Distinto de	5 != 3	True
 # >	    Mayor que	7 > 4	True
 # <	    Menor que	2 < 1	False
 # >=	Mayor o igual que	5 >= 5	True
 # <=	Menor o igual que	3 <= 2	False
+
+# Operaciones logidos
+
+# and	Y (True si ambas condiciones son True)	(5 > 3) and (2 < 4)	True
+# or	O (True si al menos una condición es True)	(5 > 10) or (3 < 4)	True
+# not	No (invierte el valor)	not True	False
