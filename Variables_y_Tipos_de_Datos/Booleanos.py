43d224e5f2b012866eddb2f6b31c2b77822cf902
Brey007 <sirope44@gmail.com>
2025-08-09 22:00:22 -0500

    Booleanos operaciones

--- Variables_y_Tipos_de_Datos/Booleanos.py
+++ Variables_y_Tipos_de_Datos/Booleanos.py
@@ -3,3 +3,10 @@ y = 3
 
 print(x > y)  # True  (5 es mayor que 3)
 print(x == y) # False (5 no es igual a 3)
+
+# ==	Igual a	5 == 5	True
+# !=	Distinto de	5 != 3	True
+# >	    Mayor que	7 > 4	True
+# <	    Menor que	2 < 1	False
+# >=	Mayor o igual que	5 >= 5	True
+# <=	Menor o igual que	3 <= 2	False
